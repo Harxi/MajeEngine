cfc7b843a98ef9f3ac90e0d09961d96eeb650767
Harxi <90150354+Harxi@users.noreply.github.com>
2022-12-05 22:14:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import gameCommands, data
 def updateScreen():
     data.x, data.y = os.get_terminal_size()
 
+player = entity.Entity(name = "Name", health = 100, maxHealth = 100, damage = 1, xp = 0, lvl = 1, coins = 0, reqxp = 250, type = "Player", items = [], weapon = data.items["BaseSword"], armor = data.items["BaseArmor"], events = data.dispatcher) # is entity
+
 while True:
     updateScreen() # set size
     print(f"{f'{chr(10)}'.join(map(lambda x: str(gameCommands.commands.showCommands[x]), gameCommands.commands.showCommands))}") # help command
